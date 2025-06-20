83eca42a7f85121b7be4425850d28cb65e3e7e39
joshua <a1922857@R910B9E6>
2025-06-20 22:39:34 +0000

    [automatic commit] 2025-06-20 22:39:34 UTC 203.63.63.42

--- part1/routes/dogwalk.js
+++ part1/routes/dogwalk.js
@@ -14,7 +14,7 @@ router.get('/dogs', async (req,res) => {
 
 router.get('/walkrequests/open', async (req,res) => {
     try {
-        var longAssQuery = 'SELECT WalkRequets.request_id, Dogs.name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username FROM WalkRequests JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id JOIN Users ON Dogs.owner_id = Users.user_id';
+        var longAssQuery = 'SELECT WalkRequests.request_id, Dogs.name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username FROM WalkRequests JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id JOIN Users ON Dogs.owner_id = Users.user_id';
         const [reqs] = await db.execute('SELECT WalkRequets.request_id, Dogs.name FROM WalkRequests');
         res.json(reqs);
     } catch (err) {
