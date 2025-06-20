14460d309a7472d14f0b0116be0cd9106cc52b60
joshua <a1922857@R910B9E6>
2025-06-20 23:06:17 +0000

    [automatic commit] 2025-06-20 23:06:17 UTC 203.63.63.42

--- part1/routes/dogwalk.js
+++ part1/routes/dogwalk.js
@@ -29,7 +29,7 @@ router.get('/walkers/summary', async (req,res) => {
         COUNT (WalkRatings.rating_id) AS totalRatings
         AVG(Walkratings.rating)
         now get completed walks by summing up how many 'completed''s there are
-        COUNT(WHEN WalkRequests.status )
+        COUNT(WHEN WalkRequests.status='completed' THEN WalkRequests.request_id END) AS 
 
         */
         const [walker] = await db.execute('SELECT Users.username, WalkRatings.rating,');
