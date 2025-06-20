f17b51432cd5720fc61bc84b2662475f99c5c12a
joshua <a1922857@R910B9E6>
2025-06-20 21:14:56 +0000

    [automatic commit] 2025-06-20 21:14:56 UTC 203.63.63.42

--- part1/db.js
+++ part1/db.js
@@ -2,7 +2,9 @@ const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
     socketPath: '/var/run/mysqld/mysqld.sock',
-    host:'127.0.0.1',
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
     
 
 });
