ad22e83c18b1c34318fe39087944be160a7b13fd
joshua <a1922857@R910B9E6>
2025-06-20 21:13:44 +0000

    [automatic commit] 2025-06-20 21:13:44 UTC 203.63.63.42

--- part1/app.js
+++ part1/app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var mysql = require('mysql2/promise');
 
 var app = express();
 
