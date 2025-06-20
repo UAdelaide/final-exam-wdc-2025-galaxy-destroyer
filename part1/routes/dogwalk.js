3e8ba098ab1b6e18f271f52b98a426a58a5cce7b
joshua <a1922857@R910B9E6>
2025-06-20 22:30:25 +0000

    [automatic commit] 2025-06-20 22:30:25 UTC 203.63.63.42

--- part1/routes/dogwalk.js
+++ part1/routes/dogwalk.js
@@ -5,7 +5,7 @@ var db = require('../db');
 // create routes for paths /dogs, /walkrequests/open, /walkers/summary
 router.get('/dogs', async (req,res) => {
     try {
-        const [dogs] = await db.execute('SELECT Dogs.name, Dogs.size, Users.username FROM Dogs JOIN Users'); // query to execute
+        const [dogs] = await db.execute('SELECT Dogs.name, Dogs.size, Users.username FROM Dogs JOIN Users ON Dogs.owner_id = Users.users_id'); // query to execute
         res.json(dogs);
     } catch (err) {
         res.status(500).json({ error: 'Could not display Dogs :(' });
