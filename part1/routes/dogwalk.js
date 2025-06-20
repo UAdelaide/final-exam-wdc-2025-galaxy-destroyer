53e0d642b6f0549fe98a9d9f0fcf9cf545bd6ecd
joshua <a1922857@R910B9E6>
2025-06-20 22:19:08 +0000

    [automatic commit] 2025-06-20 22:19:08 UTC 203.63.63.42

--- part1/routes/dogwalk.js
+++ part1/routes/dogwalk.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var db = require('../db');
 
 // create routes for paths /dogs, /walkrequests/open, /walkers/summary
-app.get('/dogs', async (req,res) => {
+router.get('/dogs', async (req,res) => {
     try {
         const [dogs] = await db.execute('SELECT * FROM Dogs'); // query to execute
         res.json(dogs);
