5b230e527fd4d6855773b57d9b6f1f0ee8424b14
joshua <a1922857@R910B9E6>
2025-06-20 21:47:39 +0000

    [automatic commit] 2025-06-20 21:47:39 UTC 203.63.63.42

--- part1/routes/dogwalk.js
+++ part1/routes/dogwalk.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db');
 
-// create routes for paths /dogs, /walkrequests/open, 
+// create routes for paths /dogs, /walkrequests/open, /walkers/summary
+
+router.get('/dogs', )
 
 module.exports = router;
