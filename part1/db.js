ac0e8a62ee27954c3211a45a1ad41e74abc3c5e1
joshua <a1922857@R910B9E6>
2025-06-20 21:39:42 +0000

    [automatic commit] 2025-06-20 21:39:42 UTC 203.63.63.42

--- part1/db.js
+++ part1/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
     // socketPath: '/var/run/mysqld/mysqld.sock',
-    host: 'localhost',
+    host: '127.0.0.1',
     user: 'root',
     password: 'root',
     database: 'DogWalkService'
