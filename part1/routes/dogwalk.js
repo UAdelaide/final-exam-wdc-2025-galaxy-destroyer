f2d521aebb586aabf2fbd1936f733a853dfd517f
joshua <a1922857@R910B9E6>
2025-06-20 23:16:04 +0000

    [automatic commit] 2025-06-20 23:16:04 UTC 203.63.63.42

--- part1/routes/dogwalk.js
+++ part1/routes/dogwalk.js
@@ -33,7 +33,7 @@ router.get('/walkers/summary', async (req,res) => {
         FROM Users
 
         */
-        const [walker] = await db.execute('SELECT Users.username AS walkerUsers, COUNT(WalkRatings.rating_id) AS totalRatings, AVG(WalkRequests) AS avgRating,COUNT()');
+        const [walker] = await db.execute('SELECT Users.username AS walkerUsers, COUNT(WalkRatings.rating_id) AS totalRatings, AVG(WalkRequests) AS avgRating,COUNT(CASE WHEN WalkRequests.status = 'completed' THEN)');
         res.status(200).json(walker);
     } catch (err) {
         res.status(500).json({ error: 'Could not show walkers summary' });
