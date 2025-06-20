686c2cf2451f104f61a5d157e1ad5b2dc90b17e9
joshua <a1922857@R910B9E6>
2025-06-20 22:39:17 +0000

    [automatic commit] 2025-06-20 22:39:16 UTC 203.63.63.42

--- part1/routes/dogwalk.js
+++ part1/routes/dogwalk.js
@@ -21,7 +21,6 @@ router.get('/walkrequests/open', async (req,res) => {
         res.status(500).json({ error: 'Could not show walk requests' });
     }
 });
-SELECT WalkRequets.request_id, Dogs.name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username FROM WalkRequests JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id JOIN Users ON Dogs.owner_id = Users.user_id;
 
 
 
