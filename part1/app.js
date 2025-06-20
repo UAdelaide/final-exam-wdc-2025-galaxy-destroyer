ef509957474114a09962f1e8fff541d59bb91fb6
joshua <a1922857@R910B9E6>
2025-06-20 22:18:13 +0000

    [automatic commit] 2025-06-20 22:18:13 UTC 203.63.63.42

--- part1/app.js
+++ part1/app.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-// app.use('/api', dogwalkRouter);
+app.use('/api', dogwalkRouter);
 
 app.get('/dogs', async (req,res) => {
     try {
