3726c8b69c984c72c8f763f2e284ef6c18092c16
joshua <a1922857@R910B9E6>
2025-06-21 04:48:28 +0000

    [automatic commit] 2025-06-21 04:48:28 UTC 203.63.63.42

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -36,7 +36,7 @@ router.get('/me', (req, res) => {
 });
 
 // POST login (dummy version)
-router.post('/login', async (req, res) => {
+router.pget('/login', async (req, res) => {
   const { username, password } = req.body;
 
   try {
