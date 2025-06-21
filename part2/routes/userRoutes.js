e1804fdeca6af1a2b981cf7d74576e2219df12d3
joshua <a1922857@R910B9E6>
2025-06-21 05:05:51 +0000

    [automatic commit] 2025-06-21 05:05:51 UTC 203.63.63.42

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -66,7 +66,7 @@ router.post('/login', async (req, res) => {
         user_id: user.user_id,
         username: user.username,
         email: user.email,
-        password: user.password
+        role: user.role
       }
     });
     // return res.json({ message: 'Login successful', user });
