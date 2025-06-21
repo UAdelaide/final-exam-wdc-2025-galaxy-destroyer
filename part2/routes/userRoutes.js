879a82f6b00e32e3f1f910927a0c85603393dedf
joshua <a1922857@R910B9E6>
2025-06-21 06:23:39 +0000

    [automatic commit] 2025-06-21 06:23:39 UTC 203.63.63.42

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -83,7 +83,7 @@ router.post('/login', async (req, res) => {
   router.get('/dogs', async (req,res) => {
     const ownerId = req.session.user_id;
 
-    const 
+    const doglistquery = '';
 
   });
 });
