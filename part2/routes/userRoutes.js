115b5cd5c21df9e2a1883089d8d10e0a4aad4c7b
joshua <a1922857@R910B9E6>
2025-06-21 06:01:18 +0000

    [automatic commit] 2025-06-21 06:01:18 UTC 203.63.63.42

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -73,7 +73,12 @@ router.post('/login', async (req, res) => {
     res.status(500).json({ error: 'Login failed' });
   }
 
-
+  // referring to the form @submit.prevent LOGOUT
+  router.post('/logout', async (req,res) => {
+    req.session.destroy; // ends session
+    // connect sid is the default session cookie from express-session
+    res.clearCookie('connect.sid');
+  });
 });
 
 module.exports = router;
