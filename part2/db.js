5747e45cd35aa18b547ffda5ea5673c9a10345b3
joshua <a1922857@R910B9E6>
2025-06-21 03:52:54 +0000

    [automatic commit] 2025-06-21 03:52:54 UTC 203.63.63.42

---
+++ part2/db.js
@@ -0,0 +1,17 @@
+onst mysql = require('mysql2/promise');
+
+const db = mysql.createPool({
+    // socketPath: '/var/run/mysqld/mysqld.sock',
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'DogWalkService'
+});
+
+// a way to ensure database is connected to express
+db.getConnection()
+  .then(() => console.log('MySQL connected!'))
+  .catch(err => console.error('MySQL could not connect to server', err));
+
+
+module.exports = db;
