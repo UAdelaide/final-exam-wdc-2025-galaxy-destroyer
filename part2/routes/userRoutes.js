51dd9ed6f7a9f696715f1efad313dd608d5e5303
joshua <a1922857@R910B9E6>
2025-06-21 04:45:44 +0000

    [automatic commit] 2025-06-21 04:45:44 UTC 203.63.63.42

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -50,7 +50,7 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-    var user = useSyncExternalStore[0]
+    var user = rows[0];
     req.session.user =  {
       user_id: this.user_id,
       username: this.username,
