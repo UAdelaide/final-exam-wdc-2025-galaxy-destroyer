e979cad9468db8acb947ebc0cc0eedbec9fefdc2
joshua <a1922857@R910B9E6>
2025-06-21 04:43:52 +0000

    [automatic commit] 2025-06-21 04:43:52 UTC 203.63.63.42

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,8 +49,9 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
+    var yu
     req.session.user = user[0]; // storing session
-    res.json({ message: 'Login successful', user: rows[0] });
+    // res.json({ message: 'Login successful', user: rows[0] });
 
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
