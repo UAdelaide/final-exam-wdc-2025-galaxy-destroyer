6554b82b06aebea86daf19d6236e1b00830a9fbd
joshua <a1922857@R910B9E6>
2025-06-21 03:40:16 +0000

    [automatic commit] 2025-06-21 03:40:16 UTC 203.63.63.42

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -178,7 +178,7 @@ function login(){
 
     let user = {
         user: document.getElementById('username').value,
-        pass: document.getElementById('password').value
+        password: document.getElementById('password').value
     };
 
     // Create AJAX Request
